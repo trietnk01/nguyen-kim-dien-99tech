358ec76a77e0554ba02dbb7ee1d7a2b6bc8fb0a9
Nguyen Kim Dien <trietnk01@gmail.com>
2024-06-26 21:04:30 +0700

    Add country-list

--- problem2/src/pages/public/HomePage.tsx
+++ problem2/src/pages/public/HomePage.tsx
@@ -1,26 +1,32 @@
-import React from "react";
-import axios from "axios";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import NumberFormat from "react-number-format";
-import { Button, Flex, Form, FormProps, Image, Input, Select, Row, Col, SelectProps } from "antd";
+import * as yup from "yup";
 import { RetweetOutlined } from "@ant-design/icons";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, Input, Select } from "antd";
+import axios from "axios";
+import React from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import styles from "@/assets/scss/home.module.scss";
+import country_list from "./country-list";
 interface IFormInput {
   amount: string;
   currency_from: string;
   currency_to: string;
+  total_exchange: number;
 }
-type FieldType = {
-  amount: string;
-  currency_from: string;
-  currency_to: string;
-};
-const dataCurrency = [
-  { value: "VND", label: "VND" },
-  { value: "USD", label: "USD" },
-  { value: "AUD", label: "AUD" }
-];
+interface ICountry {
+  value: string;
+  label: string;
+}
 const HomePage = () => {
+  const [countries, setCountries] = React.useState<ICountry[]>([]);
+  const [flagFrom, setFlagFrom] = React.useState<string>("US");
+  const [flagTo, setFlagTo] = React.useState<string>("VN");
+  const schema = yup
+    .object({
+      amount: yup.string().required("Amount required!")
+    })
+    .required();
   const {
     handleSubmit,
     control,
@@ -31,16 +37,27 @@ const HomePage = () => {
     defaultValues: {
       amount: "1,000",
       currency_from: "USD",
-      currency_to: "VND"
-    }
+      currency_to: "VND",
+      total_exchange: 0
+    },
+    resolver: yupResolver(schema)
   });
   const onSubmit: SubmitHandler<IFormInput> = async (dataForm) => {
-    let amountNumber: number = dataForm.amount ? parseInt(dataForm.amount.replace(",", "")) : 0;
+    let amountNumber: number = dataForm.amount
+      ? parseInt(dataForm.amount.toString().replace(new RegExp(",", "g"), ""))
+      : 0;
+    console.log("amountNumber = ", amountNumber);
     let urlExchange = `https://v6.exchangerate-api.com/v6/${
       import.meta.env.VITE_EXCHANGERATE_API
-    }/latest/${amountNumber}`;
+    }/latest/${dataForm.currency_from}`;
     let res = await axios.get(urlExchange);
-    console.log("res = ", res);
+    if (res && res.data && res.data.conversion_rates) {
+      const rateCurrency: number = parseFloat(
+        res.data.conversion_rates[dataForm.currency_to].toString()
+      );
+      let totalExchange: number = Math.round(amountNumber * rateCurrency);
+      setValue("total_exchange", totalExchange);
+    }
   };
   const handleAmountChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | undefined
@@ -49,9 +66,28 @@ const HomePage = () => {
   };
   const handleCurrencyFromChange = (e: any) => {
     setValue("currency_from", e ? e.toString() : "");
+    setFlagFrom((country_list as any)[e?.toString()].toString());
   };
   const handleCurrencyToChange = (e: any) => {
     setValue("currency_to", e ? e.toString() : "");
+    setFlagTo((country_list as any)[e?.toString()].toString());
+  };
+  React.useEffect(() => {
+    let countryList: ICountry[] = [];
+    for (let country in country_list) {
+      countryList.push({ value: country, label: country });
+    }
+    setCountries(countryList);
+  }, []);
+  const handleSwitchCountry = () => {
+    const currencyFrom: string = getValues("currency_from");
+    const currencyTo: string = getValues("currency_to");
+    const flagFrom: string = (country_list as any)[currencyFrom].toString();
+    const flagTo: string = (country_list as any)[currencyTo].toString();
+    setValue("currency_from", currencyTo);
+    setValue("currency_to", currencyFrom);
+    setFlagFrom(flagTo);
+    setFlagTo(flagFrom);
   };
   return (
     <div className={styles.container}>
@@ -65,14 +101,17 @@ const HomePage = () => {
               control={control}
               render={({ field }) => {
                 return (
-                  <NumberFormat
-                    placeholder="Amount *"
-                    customInput={Input}
-                    thousandSeparator={true}
-                    size="large"
-                    value={field.value}
-                    onChange={handleAmountChange}
-                  />
+                  <React.Fragment>
+                    <NumberFormat
+                      placeholder="Amount *"
+                      customInput={Input}
+                      thousandSeparator={true}
+                      size="large"
+                      value={field.value}
+                      onChange={handleAmountChange}
+                    />
+                    {errors.amount && <div className={styles.errors}>{errors.amount.message}</div>}
+                  </React.Fragment>
                 );
               }}
             />
@@ -82,7 +121,7 @@ const HomePage = () => {
               <p>From</p>
               <div className={styles.selectBox}>
                 <img
-                  src={`/${getValues("currency_from").toString()}.png`}
+                  src={`https://flagsapi.com/${flagFrom}/flat/64.png`}
                   alt="Flag"
                   width={30}
                   height={30}
@@ -96,7 +135,7 @@ const HomePage = () => {
                         <Select
                           className={styles.txtSelected}
                           size="large"
-                          options={dataCurrency}
+                          options={countries}
                           value={field.value}
                           onChange={handleCurrencyFromChange}
                         />
@@ -107,13 +146,13 @@ const HomePage = () => {
               </div>
             </div>
             <div className={styles.icon}>
-              <RetweetOutlined />
+              <RetweetOutlined onClick={handleSwitchCountry} />
             </div>
             <div className={styles.to}>
               <p>To</p>
               <div className={styles.selectBox}>
                 <img
-                  src={`/${getValues("currency_to").toString()}.png`}
+                  src={`https://flagsapi.com/${flagTo}/flat/64.png`}
                   alt="Flag"
                   width={30}
                   height={30}
@@ -127,7 +166,7 @@ const HomePage = () => {
                         <Select
                           className={styles.txtSelected}
                           size="large"
-                          options={dataCurrency}
+                          options={countries}
                           onChange={handleCurrencyToChange}
                           value={field.value}
                         />
@@ -140,7 +179,14 @@ const HomePage = () => {
           </div>
           <div className={styles.exchangeRate}>
             {getValues("amount") ? getValues("amount").toString() : ""}{" "}
-            {getValues("currency_from") ? getValues("currency_from") : ""} = 118.16{" "}
+            {getValues("currency_from") ? getValues("currency_from") : ""} ={" "}
+            {new Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD",
+              maximumFractionDigits: 0
+            })
+              .format(getValues("total_exchange"))
+              .replace("$", "")}{" "}
             {getValues("currency_to") ? getValues("currency_to") : ""}
           </div>
           <Button htmlType="submit" type="primary" size="large">
